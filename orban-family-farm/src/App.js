583142b012421572752dffc4c2d807c10c47e3c7
Ben Orban <benjamin.orban@gmail.com>
2021-09-19 16:12:20 -0500

    Adding banner and nav tabs

--- orban-family-farm/src/App.js
+++ orban-family-farm/src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
-import AppBar from '@mui/material/AppBar';
-import { Toolbar } from '@mui/material';
-import { Typography } from '@mui/material';
+import { Tab, Tabs, Box } from '@mui/material';
+import banner from './images/banner.png';
 
 function App() {
   return (
     <div>
-      <AppBar color="primary" position="static">
-          <Toolbar>
-              Orban Family Farm
-          </Toolbar>
-        </AppBar>
-    </div>
+      <Box sx={{
+        flexGrow: 1, bgcolor: 'background.paper', display: 'flex',
+        alignItems: 'center', justifyContent: 'center'
+      }} >
+        <img
+          src={banner} width="600" height="200"
+          alt="banner" />
+      </Box>
+      <Box sx={{
+        flexGrow: 1, bgcolor: 'background.paper', display: 'flex',
+        height: '50px', width: '100%', alignItems: 'center', justifyContent: 'center'
+      }} >
+        <Tabs aria-label="basic tabs example" sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tab label="Home" />
+          <Tab label="Farm History" />
+          <Tab label="Family Members" />
+          <Tab label="Contact Info" />
+          <Tab label="Blog" />
+        </Tabs>
+      </Box>
+
+    </div >
   );
 }
 
