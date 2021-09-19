20f85219f2fd00f8758639037198cc908b27ff97
Ben Orban <benjamin.orban@gmail.com>
2021-09-19 07:21:37 -0500

    Added toolbar

--- orban-family-farm/src/App.js
+++ orban-family-farm/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+      <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy variant="title"
+              color="inherit"
+            >
+              My header
+           </TypoGraphy>
+          </Toolbar>
+        </AppBar>
       </header>
     </div>
   );
