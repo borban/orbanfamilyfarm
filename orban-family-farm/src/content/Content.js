07a2048cf5ab4a3586568183b1e1e02e1b5e1b39
borban <borban@@users.noreply.github.com>
2022-08-02 15:08:53 -0500

    Finally got dynamic cards working.

--- orban-family-farm/src/content/Content.js
+++ orban-family-farm/src/content/Content.js
@@ -3,42 +3,35 @@ import Card from "../components/Card";
 import axios from "axios";
 
 function Content() {
-  const getText = () => {
-    axios.get('https://tgyfpk3h5g.execute-api.us-east-1.amazonaws.com/default/?id=10006546')
+  const getSummaries = () => {
+    axios
+      .get("https://tgyfpk3h5g.execute-api.us-east-1.amazonaws.com/default")
       .then((response) => {
-        let result = JSON.parse(response.data);
-        setText(result.document.summary);
-        console.log(result.document.summary);
+        let results = JSON.parse(response.data);
+        setSummaries(results.documents.map((document) => document.summary));
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  const [text, setText] = useState(() => getText());
+  const [summaries, setSummaries] = useState(() => getSummaries());
 
   return (
     <div className="content">
-      <Card
-        title="First Card"
-        text={text}
-        image="https://via.placeholder.com/150"
-      />
-      <Card
-        title="Second Card"
-        text={text}
-        image="https://via.placeholder.com/150"
-      />
-      <Card
-        title="Third Card"
-        text={text}
-        image="https://via.placeholder.com/150"
-      />
-      <Card
-        title="Fourth Card"
-        text={text}
-        image="https://via.placeholder.com/150"
-      />
+      {summaries ? (
+        summaries.map((summary) => {
+          return (
+            <Card
+              title="Card"
+              text={summary}
+              image="https://loremflickr.com/250/260"
+            />
+          );
+        })
+      ) : (
+        <div>No content</div>
+      )}
     </div>
   );
 }
